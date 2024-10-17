1af64ccd5d8e59678a1e5bcce71052909d1b7d64
Semih <69973566+semih-uslu@users.noreply.github.com>
2024-10-17 11:16:44 +0300

    04_loops | proje eklendi

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region For Döngüsü
+
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+
+
+            Console.Read();
+
+        }
+    }
+}
